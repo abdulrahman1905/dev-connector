1a4a1c670e909a6971c87c61afa238785298674d
Abdulrahman Olaobaju <olaobajurahmon@gmail.com>
2023-07-11 08:21:25 +0300

    Profile routes added

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+//initialize bodyParser
+app.use(express.json({ extended: false }))
+
+//connect to MongoDB
+connectDB()
+
+//define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/auth', require('./routes/api/auth'))
 
 const PORT = process.env.PORT || 5000
 
